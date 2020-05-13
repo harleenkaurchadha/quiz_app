bb2f5d04efa1fc0f311ec3ec01d9a4178de0f7e1
Harleen Kaur <harleen198.chadha@gmail.com>
2020-05-13 12:48:41 -0700

    quiz app basic with null button

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,27 @@ class MyApp extends StatelessWidget {
   //indicating to override the build functn and providing our own definition
   Widget build(BuildContext context) {
     //build is called by flutter when we try to draw something on screen
-    return MaterialApp(home: Scaffold(  //scaffold is used for creating base page design
+    return MaterialApp(
+        home: Scaffold(
+      //scaffold is used for creating base page design
       appBar: AppBar(
-        title: Center(child: Text('MY FIRST APP')),
+        title: Center(child: Text('QUIZ TIMEE!!')),
       ),
-      body: Text('THIS IS MY WELCOME PROGRAM'),
-    )
-    );
+      body: Column(children: [
+        Center(child: Text('What is your favourite colour')),
+        RaisedButton(
+          child: Text('PINK'),
+          onPressed: null,
+        ),
+        RaisedButton(
+          child: Text('BLACK'),
+          onPressed: null,
+        ),
+        RaisedButton(
+          child: Text('BLUE'),
+          onPressed: null,
+        ),
+      ]),
+    ));
   }
 }
